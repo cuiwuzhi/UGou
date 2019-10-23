44ca4585e80836ad9d4afeb0f04737625c364e9a
cuiwuzhi <cuiwuzhi@tanzhishuju.com>
2019-10-23 19:04:52 +0800

    购物车UI优化

--- src/mixins/tabs/cart.js
+++ src/mixins/tabs/cart.js
@@ -9,6 +9,16 @@ export default class extends wepy.mixin {
     // 监听商品数量变化事件
     countChanged(e) {
       this.$parent.updateGoodsCount(e.target.dataset.id, e.detail)
+    },
+    // 监听购物车列表复选框改变事件
+    statusChanged(e) {
+      const id = e.target.dataset.id // 当前点击项的商品ID
+      const status = e.detail
+      this.$parent.updateGoodsStatus(id, status)
+    },
+    // 删除商品
+    removeItem(id) {
+      this.$parent.removeGoodsById(id)
     }
   }
 
